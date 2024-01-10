3450c7a8ac3854611bd90dabc6faca926dcf09e7
Tanzim <tanzim008@gmail.com>
2024-01-10 01:23:41 -0330

    html of projects,expericne

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
-
+import Title from './utils/Title'
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <section id='projects' className='projects'>
+        <Title title="Web Projects"/>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-4">
+                    <div className="project-card">
+                        <div className="project-image">Image</div>
+                            <div className="project-info">
+                                <div className="project-header">
+                                    <div className="project-title">Develop a website using React</div>
+                                    <a href="http://"></a>
+                                </div>
+                                <div className="project-details">
+                                    <p>In the digital age, establishing an online presence is crucial for showcasing your skills and accomplishments. One effective way to achieve this is by creating a personal portfolio website. </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <div className="col-md-4"></div>
+                <div className="col-md-4"></div>
+            </div>
+        </div>
+    </section>  
   )
 }
 
--- src/components/utils/Card.jsx
+++ src/components/utils/Card.jsx
@@ -7,9 +7,13 @@ const Card = () => {
             <div className="card-body">
                 <h5 className="card-title">Customer Service Representative @ Intact Insurance</h5>
                 <h6 className="card-subtitle mb-2 text-muted">November 2022 - Present | St. Johns, Newfoundland</h6>
+                <ul>
+                    <li>Openning a new auto claim</li>
+                    <li>Assist customer with existing claimm</li>
+                    <li>Adjusting customer claims</li>
+                </ul>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="card-link">Card link</a>
-                <a href="#" className="card-link">Another link</a>
+                <a href="#" className="card-link">About Company</a>
             </div>
         </div>
     </>
