526775d5b15dbb45df9773f967167b1958b71426
Tanzim <tanzim008@gmail.com>
2024-02-08 19:46:41 +0600

    committed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import './assets/styles.scss'
-import './assets/media.scss'
+import './assets/styles.scss';
+import './assets/media.scss';
+import Social from './common/Social';
 import Navbar from './components/Navbar';
 import Slider from './components/Slider';
 import About from './components/About';
@@ -7,10 +8,13 @@ import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './common/Footer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
     <div className="App">
+      {/* <Social/> */}
       <Navbar/>
       <Slider/>
       <About/>
---
+++ src/common/Social.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHithub } from '@fortawesome/free-brands-svg-icons'
+
+
+const Social = () => {
+  return (
+    <div className='social-icons'>
+        {/* <FontAwesomeIcon icon={faGithub} />     */}
+    </div>
+  )
+}
+
+export default Social;
