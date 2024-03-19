3cbbaad2a7abdd368410a7152947c466c0c0ca96
Tanzim <tanzim008@gmail.com>
2024-03-18 23:28:17 -0230

    functionality added : filtered project category

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,50 +8,55 @@ import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Projects = () => {
   const [projectData, setProjectData] = useState(data);
-  const [currentTab, setCurrentTab] = useState('');
-  const [projectTool, setProjectTool] = useState([]);
+  const [filteredProjectData, setFilteredProjectData] = useState([...projectData]);
 
-  useEffect(() => {
-    // Use useEffect to update projectTool when projectData changes
-    const tools = projectData.reduce((acc, project) => {
-      return [...acc, ...project.tools];
-    }, []);
-    setProjectTool(tools);
-  }, [projectData]); // Dependency array ensures useEffect runs when projectData changes
+  const filteredCategory = [...new Set(projectData.map((project) => {
+    return project.category;
+  }))];
 
+  const handleCategoryChange = (getCurrentCategory) => {
+    setFilteredProjectData(projectData.filter((project) => {
+      return project.category === getCurrentCategory;
+    }))
+  }
   return (
     <section id='projects' className='projects'>
       <Title title="Web Projects" />
       <div className="project-tabs">
         <div className="project-tab">
           {/* Render project tools */}
-          {projectTool.map((tool, index) => (
-            <p key={index}>{tool}</p>
-          ))}
+          <p onClick={()=>{setFilteredProjectData(projectData)}}>All</p>
+          {filteredCategory.map((category) => {
+            return (
+              <p key={category} onClick={() => { handleCategoryChange(category) }}>{category}</p>
+            )
+
+          })}
         </div>
       </div>
       <div className="projects-card">
         {/* Render project cards */}
-        {projectData.map((project, index) => (
-          <div className="project-card" key={index}>
-            <div className="project-name">
-              <h2>{project.title}</h2>
+        {
+          filteredProjectData.map((project, index) => (
+            <div className="project-card" key={index}>
+              <div className="project-name">
+                <h2>{project.title}</h2>
+              </div>
+              <div className="project-details">
+                <p>{project.details}</p>
+              </div>
+              <div className="project-tools">
+                {project.tools.map((tool, index) => (
+                  <p key={index}>{tool}</p>
+                ))}
+              </div>
+              <div className="project-links">
+                <a href={project.link} target='_blank' rel="noreferrer">
+                  VIEW PROJECT <FontAwesomeIcon icon={faExternalLinkAlt} />
+                </a>
+              </div>
             </div>
-            <div className="project-details">
-              <p>{project.details}</p>
-            </div>
-            <div className="project-tools">
-              {project.tools.map((tool, index) => (
-                <p key={index}>{tool}</p>
-              ))}
-            </div>
-            <div className="project-links">
-              <a href={project.link} target='_blank' rel="noreferrer">
-                VIEW PROJECT <FontAwesomeIcon icon={faExternalLinkAlt} />
-              </a>
-            </div>
-          </div>
-        ))}
+          ))}
       </div>
     </section>
   );
--- src/data/projects.js
+++ src/data/projects.js
@@ -4,6 +4,7 @@ export const data = [
         title: 'FULL WEBSITE WITH WORDPRESS FOR HERITAGE ARCHIVES',
         details: 'Wordpress based full website to showcase the collection of the archives. Developed a catalog classification to show the types of collection the archive has. Users can also view the publications which are made with the Custom post type plugin in wordpress.',
         tools: ['Wordpress', 'Elementor'],
+        category: 'WordPress',
         link: 'https://heritage-archives.com',
         github: false,
         githubLink: ''
@@ -13,6 +14,7 @@ export const data = [
         title: 'E-Commerce Website for Lookshop BD',
         details: 'This is a complete woocommerce based website for selling cosmetic products in Bangladesh. The business has over 2000 products and they have started selling through this eCommerce. I used the woodmart theme to develop this site',
         tools: ['Wordpress', 'Elementor', 'Woocommerce'],
+        category: 'ECommerce',
         link: 'https://lookshopbd.com',
         github: false,
         githubLink: ''
@@ -22,6 +24,7 @@ export const data = [
         title: 'Laptop Review Event Landing Page',
         details: 'It was an event for Asus Bangladesh. The user had to fill up thier basic information and show the proof of thier laptop review by uploading the sceenshot and the link. The web form I developed was able to collect over 100 reviews. There was also a backend to check the sumitted reviews and mark them. ',
         tools: ['PHP', 'Laravel', 'JavaScript', 'Bootstrap'],
+        category: 'Laravel',
         link: 'https://www.behance.net/gallery/129215805/USER-GENERATED-REVIEW-CAMPAIGN-MICROSITE',
         github: false,
         githubLink: ''
@@ -31,6 +34,7 @@ export const data = [
         title: 'Full Website for Contrivance',
         details: 'A business to sell electronic products in Bangladesh. I created a form to provide the product registration feature for the customers. Also, I kept a store locator table in the site. I used the free "Astro theme to build the website."',
         tools: ['Wordpress','Elementor'],
+        category: 'WordPress',
         link: 'https://contrivance-bd.com/',
         github: false,
         githubLink: ''
@@ -40,6 +44,7 @@ export const data = [
         title: 'Quiz App for ASUS',
         details: 'The app had 5 quizes containing 20 points and the user with the highest points would get a free ticket for the laptop fair. It had a backend with admin access to view the users who participated in the quiz. The app also generated unique tickets for the winners."',
         tools: ['PHP','JavaScript','Bootstrap','MySQL'],
+        category: 'PHP',
         link: 'https://contrivance-bd.com/',
         github: false,
         githubLink: ''
