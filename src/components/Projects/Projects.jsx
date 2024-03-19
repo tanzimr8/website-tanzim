9ab56373311737cd4c4bae3a4ccd7984a5c6c5e9
Tanzim <tanzim008@gmail.com>
2024-03-18 23:36:34 -0230

    fixed css in filtered tab

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,6 +8,7 @@ import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Projects = () => {
   const [projectData, setProjectData] = useState(data);
+  const [selectedCategory, setSelectedCategory] = useState('All');
   const [filteredProjectData, setFilteredProjectData] = useState([...projectData]);
 
   const filteredCategory = [...new Set(projectData.map((project) => {
@@ -18,6 +19,7 @@ const Projects = () => {
     setFilteredProjectData(projectData.filter((project) => {
       return project.category === getCurrentCategory;
     }))
+    setSelectedCategory(getCurrentCategory);
   }
   return (
     <section id='projects' className='projects'>
@@ -25,10 +27,13 @@ const Projects = () => {
       <div className="project-tabs">
         <div className="project-tab">
           {/* Render project tools */}
-          <p onClick={()=>{setFilteredProjectData(projectData)}}>All</p>
+          <p className={selectedCategory === 'All' ? 'active' : null} onClick={() => {
+            setSelectedCategory('All');
+            setFilteredProjectData(projectData)
+          }}>All</p>
           {filteredCategory.map((category) => {
             return (
-              <p key={category} onClick={() => { handleCategoryChange(category) }}>{category}</p>
+              <p className={selectedCategory === category ? 'active' : null} key={category} onClick={() => { handleCategoryChange(category) }}>{category}</p>
             )
 
           })}
