472f5a7e7e5baf04c88a6dfffed1c082b4acb9f6
Tanzim <tanzim008@gmail.com>
2024-07-20 15:39:05 -0230

    updated about me information

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 import Title from './utils/Title';
 import { Button } from './utils/Button';
 import TanzimImage from './../assets/images/tanzim.webp';
-import Resume from './../assets/Resume.pdf'; 
+import Resume from './../assets/Resume.pdf';
 const About = () => {
   return (
     <section id='about' className='about'>
-        <div className="my-photo">
-          <img src={TanzimImage} alt="Photo of Tanzim Rahman" />
-        </div>
-        <div className="about-me">
-            <Title title='About me'/>
-            <p><strong>I would love to introduce </strong> me as a traveler. I have been Traveling from the ins and outs of Bangladesh to the beautiful hilly towns besides the Atlantic ocean in Newfoundland, Canada. After travelling my passion comes for coding.<br/>
-            <strong>I have been working </strong> with the web technologies such as JavaScript(ES6), HTML, CSS, Bootstrap, React etc. I have a 4 years of professional experience in Web Development. In my workplace I used to work with WorpdPress, PHP and Laravel. But, at this moment I am training myself as Frontend Engineer. My expertise is in Javascript, React JS, Bootstrap and HTML/CSS<br/>
-            <strong>After moving to Canada</strong> I started working in Sobeys as a Sales Cashier. Then , I worked as a Customer Service Representative in the auto claims department in Intact insurance company. It had been a very new experience for me and It was exciting and helped me to improve my problem solvi ng ability.</p>
-            <a href={Resume} target='_blank' download  ><Button text='Download my CV'/></a>
-        </div>
+      <div className="my-photo">
+        <img src={TanzimImage} alt="Photo of Tanzim Rahman" />
+      </div>
+      <div className="about-me">
+        <Title title='About me' />
+        <p>I am an avid traveler, having explored the diverse landscapes of Bangladesh and the picturesque hilly towns along the Atlantic Ocean in Newfoundland, Canada. My passion for travel is matched only by my love for <strong>coding</strong>. <br/><br/>
+        With 4 years of professional experience in <strong>web development</strong>, I have honed my skills in <strong>JavaScript (ES6), HTML, CSS, Bootstrap, and React. </strong> In my previous roles, I worked extensively with WordPress, PHP, and Laravel. Currently, I am training to become a Frontend Engineer, focusing on <strong> JavaScript, React.js, Bootstrap, and HTML/CSS. </strong> <br/> <br/>
+
+          After relocating to Canada, I started working at Sobeys as a Sales Cashier and later transitioned to a Customer Service Representative role in the auto claims department at Intact Insurance. This experience was both new and exciting, enhancing my problem-solving abilities.</p>
+        <a href={Resume} target='_blank' download  ><Button text='Download my CV' /></a>
+      </div>
     </section>
   )
 }
